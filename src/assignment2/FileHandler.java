e58b9989b93478fdaca7c3b2abc14bb153dbb7c7
ym-4 <209573660+ym-4@users.noreply.github.com>
2026-09-20 16:42:38 +0800

    Clean up comments in FileHandler.java
    Removed unnecessary author and student information comments from FileHandler.java to streamline the code.

--- src/assignment2/FileHandler.java
+++ src/assignment2/FileHandler.java
@@ -6,21 +6,6 @@ package assignment2;
 
 import java.io.*;
 
-/**
- *
- * @author User
- * 
- * Student 1: 
- * Class: DIT2B21
- * Student ID: 2537159
- * Name: PAY YU MI
- * 
- * Student 2: 
- * Class: DIT2B21
- * Student ID: 2523398
- * Name: TAN MU LING
- * 
- */
 public class FileHandler {
     // Stores student and company data
     private Student[] students;
